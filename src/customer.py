af40518234c5fad2fe90decb0926ba0557f76832
Christian E. Mercado <christian.erick.mf@gmail.com>
2026-02-17 21:57:32 -0600

    feat: implement Customer class structure and imports
    Create base class for Customer management including CRUD method
    like create_customer, delete_customer, display_customer, and
    modify_customer.

--- src/customer.py
+++ src/customer.py
@@ -7,3 +7,34 @@ with JSON file persistence.
 Author: A00841954 Christian Erick Mercado Flores
 Date: February 2026
 """
+
+import json
+import os
+
+from .utils.constants import (
+    ERROR_PREFIX,
+    SUCCESS_PREFIX,
+    WARNING_PREFIX,
+    HOTELS_FILE,
+)
+
+class Customer:
+
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def create_customer():
+        pass
+
+    @staticmethod
+    def delete_customer():
+        pass
+
+    @staticmethod
+    def display_customer():
+        pass
+
+    @staticmethod
+    def modify_customer():
+        pass
