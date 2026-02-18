de931fc8de6a49bfb5c83120d468152e129df6f2
Christian E. Mercado <christian.erick.mf@gmail.com>
2026-02-18 17:37:38 -0600

    test: set up unit test boilerplate for Hotel class
    Initialize TestHotel class using unittest framework and define
    setUp method for future test isolation logic.

--- tests/unit/hotel_test.py
+++ tests/unit/hotel_test.py
@@ -8,3 +8,17 @@ Author: A00841954 Christian Erick Mercado Flores
 Date: February 2026
 """
 
+import unittest
+from src.hotel import Hotel, _load_hotels, _save_hotels
+
+
+class TestHotel(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
